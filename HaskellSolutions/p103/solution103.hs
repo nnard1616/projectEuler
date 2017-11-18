dd4e18a4b13227b84d98faf33818e26299153105
Nathan <nnard1616@gmail.com>
2017-11-17 22:05:08 -0600

    added comments to solution103.hs

--- HaskellSolutions/p103/solution103.hs
+++ HaskellSolutions/p103/solution103.hs
@@ -1,7 +1,8 @@
 module Solution103 where
 import Data.List
 
-deltas = mapM (const [-4..4]) [1..7] --how does mapM work?
+deltas = mapM (const [-4..4]) [1..7] --7 dimension displacement vectors with integer components varying from -4..4:
+                                     -- [ [-4,-4,-4,-4,-4,-4,-4], [-4,-4,-4,-4,-4,-4,-3],...]
 
 norm :: Floating r => [r] -> r
 norm v = sqrt $ sum $ map (^2) v
@@ -11,5 +12,10 @@ norms xs ys  | norm xs < norm ys = LT
              | norm ys < norm xs = GT 
              | otherwise         = EQ
 
-
-
+-- GOAL:  Sort list of vectors by their norms (magnitudes)
+-- running "sortBy norms norm" yields the following error message:
+-- <interactive>:66:8:
+--     No instance for (Floating Integer) arising from a use of `norms'
+--     In the first argument of `sortBy', namely `norms'
+--     In the expression: sortBy norms deltas
+--     In an equation for `it': it = sortBy norms deltas
