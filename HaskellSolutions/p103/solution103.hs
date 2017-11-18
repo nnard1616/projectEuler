4225f39f0d5280038e25d21e50a7f56be22abee3
Nathan <nnard1616@gmail.com>
2017-11-17 21:53:26 -0600

    added type signatures to solution103.hs

--- HaskellSolutions/p103/solution103.hs
+++ HaskellSolutions/p103/solution103.hs
@@ -3,8 +3,10 @@ import Data.List
 
 deltas = mapM (const [-4..4]) [1..7] --how does mapM work?
 
+norm :: Floating r => [r] -> r
 norm v = sqrt $ sum $ map (^2) v
 
+norms :: (Floating a, Ord a) => [a] -> [a] -> Ordering
 norms xs ys  | norm xs < norm ys = LT
              | norm ys < norm xs = GT 
              | otherwise         = EQ
